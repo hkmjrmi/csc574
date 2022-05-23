b3bbf84209f1ab199bc168ad3440eff2473e2bd2
Hakim Jurimi <77281311+hkmjrmi@users.noreply.github.com>
2022-05-23 10:55:53 +0800

    lab 1

---
+++ lab-01/lab-01.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lab 1 | My Biodata</title>
+</head>
+   
+<body style="background-color: #4b4bc9; text-transform: uppercase;">
+    <div style="background-color: #dfdfe6; margin: auto; width: 50%; border: 3px solid black; border-radius: 7px; padding: 10px; text-align: center;">
+    <h1>My Biodata</h1>
+    <img src="profile.jpg" width="350" height="300" style="border: 1px solid black;">
+    <img src="family.jpg" width="350" height="300" style="border: 1px solid black;">
+    <p><b>Name</b></p>
+    <p>Muhammad Abdul Hakim bin Jurimi</p>
+    <p><b>Student ID</b></p>
+    <p>2020872304</p>
+    <p><b>Address</b></p>
+    <p>Banting, Selangor</p>
+    <p><b>Telephone No:</b></p>
+    <p>01112307067</p>
+    <p><b>CGPA</b></p>
+    <p>3.33</p>
+    </div>
+
+
+</body>
+</html>
